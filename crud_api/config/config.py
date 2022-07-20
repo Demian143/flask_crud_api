f717e416fe28eb7e0a006ae5d27b8ac5d14be99c
demian <patrickxavier140@gmail.com>
2022-07-19 21:24:17 -0300

    setting examples for endpoints, models and db config

--- crud_api/config/config.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-import os 
-
-engine = create_engine("postgresql://{os.environ['USER']}@localhost:5432/some-postgres")
-
-engine.connect()
--- crud_api/index.py
+++ crud_api/index.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-
+# app and db config
 app = Flask(__name__)
+app.config['SQLALCHMY_DATABASE_URI'] = os.environ['SQLALCHMY_DATABASE_URI']
+db = SQLAlchemy(app)
 
+# example of how to make models
+class User(db.model):
+    pass
+
+# api endpoints example
 @app.route('/', methods=['GET', 'POST','PUT','DELETE'])
 def api():
     if request.methd == 'GET':
@@ -13,4 +22,6 @@ def api():
         pass
     if request.methd == 'DELETE':
         pass
-            
+
+if __name__ == '__main__':
+    app.run(debug=True)
