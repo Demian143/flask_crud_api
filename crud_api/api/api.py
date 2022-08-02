f2ae82b1515a80f029492d53d6d1410c4fbaa27e
Demain143 <patrickxavier140@gmail.com>
2022-08-02 19:07:24 -0300

    separating the responsability of each http method into dedicated functions

--- crud_api/api/api.py
+++ crud_api/api/api.py
@@ -1,29 +1,31 @@
-from email.mime import message
 from flask import Blueprint, request, jsonify
 from .methods import GetUser, post, delete
 
 crud_api = Blueprint('crud_api', __name__)
 
 
-@crud_api.route('/crud', methods=['GET', 'POST', 'DELETE'])
-def operations():
-    if request.method == 'GET':
-        id = request.args.get('id', type=int)
-        if id:
-            return GetUser().get_by_id(id)
+@crud_api.route('/crud', methods=['GET'])
+def get_operations():
+    id = request.args.get('id', type=int)
+    if id:
+        return GetUser().get_by_id(id)
 
-        email = request.args.get('email', type=str)
-        if email:
-            return GetUser().get_by_email(email)
+    email = request.args.get('email', type=str)
+    if email:
+        return GetUser().get_by_email(email)
 
-        return jsonify(message=("You are maybe passing the wrong parameters in the request, please use the parameter 'id' to search for a user"), status=400)
+    return jsonify(message=("You are maybe passing the wrong parameters in the request, please use the parameter 'id' to search for a user"), status=400)
 
-    if request.method == 'POST':
-        name = request.args.get('name', type=str)
-        email = request.args.get('email', type=str)
 
-        return post(name, email)
+@crud_api.route('/crud', methods=['POST'])
+def post_operations():
+    name = request.args.get('name', type=str)
+    email = request.args.get('email', type=str)
 
-    if request.method == 'DELETE':
-        id = request.args.get('id', type=int)
-        return delete(id)
+    return post(name, email)
+
+
+@crud_api.route('/crud', methods=['DELETE'])
+def delete_operations():
+    id = request.args.get('id', type=int)
+    return delete(id)
