5e6f14e6b65c7fe501069d46dbe074394eaae480
Demain143 <patrickxavier140@gmail.com>
2022-08-04 13:37:57 -0300

    adding more tests

--- crud_api/api/tests/tests.py
+++ crud_api/api/tests/tests.py
@@ -1,21 +1,46 @@
 import unittest
 from crud_api.app import create_app
 
+""" I need to: check if the right parameters is beeing passed"""
+
 
 class TestGet(unittest.TestCase):
+    tester = create_app().test_client()
+
     def test_get_user_by_email(self):
-        """Create a user and search for it,
+        """Create a user and search for by email,
         check if the json is equal the expected response."""
-        tester = create_app().test_client(self)
+        self.tester.post(
+            '/crud?name=Elliot Anderson&email=black.hat666@protonmail.com')
 
-        tester.post(
-            'http://localhost:5000/crud?name=Elliot Anderson&email=black.hat666@protonmail.com')
-
-        get_new_user = tester.get(
-            'http://localhost:5000/crud?email=black.hat666@protonmail.com')
+        get_new_user = self.tester.get(
+            '/crud?email=black.hat666@protonmail.com')
 
         self.assertEqual(
             get_new_user.data, b'{"email":"black.hat666@protonmail.com","id":1,"username":"Elliot Anderson"}\n')
+
+    def test_get_user_by_id(self):
+        """Create a user and search by id,
+        check if the json is equal the expected response."""
+        self.tester.post(
+            '/crud?name=Elliot Anderson&email=black.hat666@protonmail.com')
+
+        get_new_user = self.tester.get(
+            '/crud?id=1')
+
+        self.assertEqual(
+            get_new_user.data, b'{"email":"black.hat666@protonmail.com","id":1,"username":"Elliot Anderson"}\n')
+
+    def test_get_unexisting_user(self):
+        get_unexisting_user = self.tester.get('/crud?id=2')
+
+        self.assertEqual(get_unexisting_user.data,
+                         b'{"message":"user don\'t exists","status":400}\n')
+
+    def test_passing_email_in_id(self):
+        wrong_query = self.tester.get('/crud?id=black.hat666@protonmail.com')
+        self.assertEqual(
+            wrong_query.data, b'{"message":"You are maybe passing the wrong parameters in the request, please use the parameter \'id\' to search for a user","status":400}\n')
 
 
 if __name__ == '__main__':
